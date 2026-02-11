d451a88f5115de21fcf8758ed7f7dd675dc1e404
rechenberger@hiberno.de <rechenberger@hiberno.de>
2026-02-11 16:08:33 +0100

    format code

--- src/main/java/de/krec/krecsafe/KrecsafeApplication.java
+++ src/main/java/de/krec/krecsafe/KrecsafeApplication.java
@@ -9,7 +9,4 @@ public class KrecsafeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KrecsafeApplication.class, args);
 	}
-
-    // TODO wo kann ich bei mir die FileUnreadableEvents ausgeben?
-
 }
